69e5178f1a5eaad7f300b1082dbc714bb67085e6
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-08-05 21:52:51 +0200

    Elias; HSLToRGB changed to HSLToColor

--- libs/numbers.js
+++ libs/numbers.js
@@ -1,6 +1,13 @@
 var $ = $ || {};
 $.GJSLibNumbers = {
 	
+	serializeBinary: function(binary) {
+		let serialized = "";
+		for (let i = 0; i < binary.length; i++)
+			serialized = serialized + binary[i];
+		return serialized;
+	},
+	
 	toBinary: function(number, powStart) {
 		let pow = powStart ? powStart : Math.pow(2, Math.floor(Math.log(number) / Math.log(2)));
 		let result = [];
@@ -18,6 +25,31 @@ $.GJSLibNumbers = {
 			if (binary[i] == 1)
 				result += pow;
 			pow = pow >> 1;
+		}
+		return result;
+	},
+	
+	toEliasGamma: function(number) {
+		let binary = $.GJSLibNumbers.toBinary(number);
+		let result = [];
+		for (let i = 0; i < binary.length - 1; i++)
+			result[i] = 0;
+		return result.concat(binary);
+	},
+	
+	toEliasDelta: function(number) {
+		let n = Math.floor(Math.log(number) / Math.log(2));
+		let binary = $.GJSLibNumbers.toBinary(number).splice(1);
+		let result = $.GJSLibNumbers.toEliasGamma(n + 1);
+		return result.concat(binary);
+	},
+		
+	toEliasOmega: function(number) {
+		let result = [0];
+		while (number > 1) {			
+			let binary = $.GJSLibNumbers.toBinary(number);			
+			result = binary.concat(result);
+			number = binary.length - 1;
 		}
 		return result;
 	}
