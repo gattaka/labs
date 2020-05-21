407b0464f3626af011b39e8b2ec12db35a3d68bc
Hynek Uhlíř <hynek.uhlir@gmail.com>
2020-05-21 21:29:52 +0200

    isogame/js/Button.js switch/hover fixed

--- isogame/js/Button.js
+++ isogame/js/Button.js
@@ -3,21 +3,22 @@ $.GIsoGame = $.GIsoGame || {};
 $.GIsoGame.Button = {		
 	create: function(ctx, text, onClick) {
 		let cfg = $.GIsoGame.Configuration;
-		let button = {};
-		button.backgroundColor = cfg.uiAreaColor;
-		button.borderColor = cfg.uiBorderColor;
-		button.fontColor = cfg.uiFontColor;
-		button.fontSize = cfg.uiButtonFontSize;
+		let button = {			
+			highlighted: false,
+			pushed: false,
+			visible: true,
+			switched: false,
+			backgroundColor: cfg.uiAreaColor,
+			borderColor: cfg.uiBorderColor,
+			fontColor: cfg.uiFontColor,
+			fontSize: cfg.uiButtonFontSize,
+			highlightBackgroundColor: "#226",
+			highlightBorderColor: "#337",
+			highlightFontColor: "#99c",
+			text: text,
+		};		
 
-		button.highlightBackgroundColor = "#226";
-		button.highlightBorderColor = "#337";
-		button.highlightFontColor = "#99c";
-
-		button.highlighted = false;
-		button.pushed = false;
-		button.visible = true;
-		ctx.font = button.fontSize + "px Monospace";		
-		button.text = text;
+		ctx.font = button.fontSize + "px Monospace";				
 		button.width = ctx.measureText(button.text).width + cfg.uiSpacing * 2;
 		button.height = button.fontSize / 2 + cfg.uiSpacing * 2;
 		button.onMouse = function(movement, mouseBtn, down, isInBounds, uiListener) {
@@ -33,13 +34,9 @@ $.GIsoGame.Button = {
 			return false;
 		};
 		button.onDrag = function(movement, isInBounds, uiListener) {
-			if (isInBounds) 
-				document.body.style.cursor = "pointer";
-			if (!uiListener.highlighted) {
-				uiListener.backgroundColor = isInBounds ? uiListener.highlightBackgroundColor : cfg.uiAreaColor;
-				uiListener.borderColor = isInBounds ? uiListener.highlightBorderColor : cfg.uiBorderColor;
-				uiListener.fontColor = isInBounds ? uiListener.highlightFontColor : cfg.uiFontColor;
-			}
+			if (isInBounds)
+				document.body.style.cursor = "pointer";					
+			uiListener.highlighted = isInBounds;
 			return false;
 		};		
 		return button;	
@@ -47,13 +44,14 @@ $.GIsoGame.Button = {
 	
 	draw: function(ctx, button) {		
 		let cfg = $.GIsoGame.Configuration;
-		ctx.fillStyle = button.highlighted ? button.highlightBackgroundColor : button.backgroundColor;
+		let light = button.switched || button.highlighted;
+		ctx.fillStyle = light ? button.highlightBackgroundColor : button.backgroundColor;
 		ctx.fillRect(button.x, button.y, button.width, button.height);			
-		ctx.fillStyle = button.highlighted ? button.highlightBorderColor : button.borderColor;		
+		ctx.fillStyle = light ? button.highlightBorderColor : button.borderColor;		
 		ctx.fillRect(button.x + cfg.uiBorder, button.y + cfg.uiBorder, button.width - cfg.uiBorder * 2, button.height - cfg.uiBorder * 2);
 
 		ctx.font = button.fontSize + "px Monospace";
-		ctx.fillStyle = button.highlighted ? button.highlightFontColor : button.fontColor;
+		ctx.fillStyle = light ? button.highlightFontColor : button.fontColor;
 		ctx.fillText(button.text, button.x + cfg.uiSpacing, button.y + button.height - 9);
 	}
 };
